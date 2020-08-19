6252c41c00830799beb3a8cd6fd1a95dc1ff4991
przemekbarczyk <przemek.barczyk@gmail.com>
2020-08-19 19:12:49 +0200

    Utworzono klasę LoginMenuController
    Przyjmuje w konstruktorze obiekt Widoku i Modelu którę są potrzebne do obsługi eventów.

---
+++ src/LoginMenuController.java
@@ -0,0 +1,11 @@
+public class LoginMenuController {
+
+    private LoginMenuView theView;
+    private LoginMenuModel theModel;
+
+    public LoginMenuController(LoginMenuView theView, LoginMenuModel theModel) {
+
+        this.theView = theView;
+        this.theModel = theModel;
+    }
+}
