edeeee94cd09224da33b193642a23b0495c947b5
przemekbarczyk <przemek.barczyk@gmail.com>
2020-08-20 13:55:20 +0200

    Zmodyfikowano klasę LoginMenuModel
    Dodano metody które pobierają i przetwarzają dane.

--- src/LoginMenuModel.java
+++ src/LoginMenuModel.java
@@ -1,6 +1,26 @@
 public class LoginMenuModel {
 
+    private boolean rememberPassword;
+    private String username;
+    private String password;
+
     public LoginMenuModel() {
 
+        rememberPassword = false;
+        username = "";
+        password = "";
+    }
+
+    public void handleCheckBox(boolean isSelected) {
+
+        rememberPassword = isSelected;
+        System.out.println(rememberPassword);
+    }
+
+    public void handleLoginButton(String username, String password) {
+
+        this.username = username;
+        this.password = password;
+        System.out.println(username + " " + password);
     }
 }
