5316610a50b0a54a8a4c63282f1aa82057c05bdd
przemekbarczyk <przemek.barczyk@gmail.com>
2020-08-20 13:20:48 +0200

    Zmodyfikowano klasę LoginMenuView
    W konstuktorze tworzony jest interfejs graficzny i zapisywany do zmiennej klasy Scene. Metoda getScene() pozwala pobrać tak zaaranżowaną Scene.
    Dwa z dodanych komponentów posiadają EventHandlera w postaci klasy LoginMenuController. W tym celu dodano dla nich gettery oraz dla pobierania niezbędnych danych.

--- src/LoginMenuView.java
+++ src/LoginMenuView.java
@@ -1,9 +1,85 @@
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.geometry.HPos;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
+import javafx.scene.Scene;
+import javafx.scene.control.*;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.GridPane;
+import javafx.scene.text.Font;
+import javafx.scene.text.Text;
+
 public class LoginMenuView {
 
-    private LoginMenuController theController;
+    private Scene scene;
+    private TextField usernameField;
+    private TextField passwordField;
+    private CheckBox checkBox;
+    private Button loginButton;
 
     public LoginMenuView() {
 
-        theController = new LoginMenuController(this, new LoginMenuModel());
+        // konfiguracja głównego Panelu
+        GridPane mainPane = new GridPane();
+        mainPane.setAlignment(Pos.CENTER);
+        mainPane.setPadding(new Insets(50));
+        mainPane.setHgap(25);
+        mainPane.setVgap(25);
+
+        // dodanie komponentów do głownego Panelu
+        Text menuNameText = new Text("PLEASE LOG IN");
+        menuNameText.setFont(new Font("Default", 24));
+        GridPane.setHalignment(menuNameText, HPos.CENTER);
+        mainPane.add(menuNameText, 0, 0, 2, 1);
+
+        Label usernameLabel = new Label("Username");
+        mainPane.add(usernameLabel, 0, 1);
+
+        usernameField = new TextField();
+        mainPane.add(usernameField, 1, 1);
+
+        Label passwordLabel = new Label("Password");
+        mainPane.add(passwordLabel, 0, 2);
+
+        passwordField = new PasswordField();
+        mainPane.add(passwordField, 1, 2);
+
+        BorderPane lowerPane = new BorderPane();
+        mainPane.add(lowerPane, 0, 3, 2, 1);
+
+        checkBox = new CheckBox("Remember me");
+        lowerPane.setLeft(checkBox);
+
+        loginButton = new Button("login");
+        lowerPane.setRight(loginButton);
+
+        scene = new Scene(mainPane);
+    }
+
+    public void addListeners(EventHandler<ActionEvent> listener) {
+
+        checkBox.setOnAction(listener);
+        loginButton.setOnAction(listener);
+    }
+
+    public Scene getScene() {
+        return scene;
+    }
+
+    public Button getLoginButton() {
+        return loginButton;
+    }
+
+    public CheckBox getCheckBox() {
+        return checkBox;
+    }
+
+    public String getUsernameFieldText() {
+        return usernameField.getText();
+    }
+
+    public String getPasswordFieldText() {
+        return passwordField.getText();
     }
 }
